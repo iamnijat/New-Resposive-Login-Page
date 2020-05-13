6563ccba3449f63c29964ce767ba1df6a1375f91
Nijat Namazzade <42466886+nijatnamazzade@users.noreply.github.com>
2020-05-14 03:07:44 +0400

    Add intitial Login Page website

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+
+new TypeIt("#simpleUsage", {
+  strings: "Login Page",
+  speed: 100,
+  waitUntilVisible: true
+}).go();
